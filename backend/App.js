8777416b60dc1fe61058d9931dd2b82be58c16cd
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2025-07-17 18:14:14 +0530

    Created structure for register,login,update-profile,get-profile,users

--- backend/App.js
+++ backend/App.js
@@ -8,6 +8,51 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// *************** Register *************** //
+app.post("/register", (request, response) => {
+    response.json(
+        {
+            "status": "Successfully Registered"
+        }
+    )
+})
+
+// *************** Login *************** //
+app.post("/login", (request, response) => {
+    response.json(
+        {
+            "status": "Successfully Logined"
+        }
+    )
+})
+
+// *************** Update Profile *************** //
+app.put("/update-profile", (request, response) => {
+    response.json(
+        {
+            "status": "Profile Updated Successfully"
+        }
+    )
+})
+
+// *************** Get Profile *************** //
+app.post("/view-profile", (request, response) => {
+    response.json(
+        {
+            "status": "Profile Viewed Successfully"
+        }
+    )
+})
+
+// *************** View All Profile *************** //
+app.post("/users", (request, response) => {
+    response.json(
+        {
+            "status": "Successfully Fetched All Users"
+        }
+    )
+})
+
 app.listen(4000, () => {
     console.log("Server is Running <3")
 })
